10fc13bc9c0d1ab86f8a915fad8b2fcf5155fca0
Artem Mygaiev <joculator@gmail.com>
2019-03-30 11:27:25 +0200

    Cleanup constants and implement BULK labeling

--- scritps/fl2k.lua
+++ scritps/fl2k.lua
@@ -2,15 +2,25 @@
 
 fl2k_proto = Proto("fl2k", "FL2000 Protocol")
 
-CONTROL_UNKNOWN = 0xFFFF
-INTERRUPT_AV = 0x10
+InterfaceClass = {
+    UNKNOWN = 0xFFFF,
+    AV = 0x10,
+}
 
-local f_stage = Field.new("usb.control_stage")
-local stage_types = {
+TransferType = {
+    INTERRUPT = 1,
+    CONTROL = 2,
+    BULK = 3,
+}
+
+ControlTransferStage = {
     SETUP = 0,
     DATA = 1,
     STATUS = 2,
 }
+
+local f_transfer = Field.new("usb.transfer_type")
+local f_stage = Field.new("usb.control_stage")
 local op_types = {
     [0x40] = "Read",
     [0x41] = "Write",
@@ -23,12 +33,12 @@ f.f_irq = ProtoField.uint32("fl2k.irq", "Interrupt", base.DEC)
 
 function fl2k_proto.dissector(buffer, pinfo, tree)
     local t_fl2k = tree:add(fl2k_proto, buffer())
+    local transfer = f_transfer()
 
-    if (pinfo.match_uint == CONTROL_UNKNOWN) then
-        pinfo.cols["info"]:set("FL2000 Registers")
+    if (transfer.value == TransferType.CONTROL) then
         local stage = f_stage()
-
-        if (stage.value == stage_types.SETUP) then
+        pinfo.cols["info"]:set("FL2000 Registers")
+        if (stage.value == ControlTransferStage.SETUP) then
             -- For future use
             local reg_op = buffer(0, 1):uint()
             local reg_addr = buffer(3, 2):le_uint()
@@ -36,23 +46,31 @@ function fl2k_proto.dissector(buffer, pinfo, tree)
             t_fl2k:add(f.f_reg_op, buffer(0, 1))
             t_fl2k:add_le(f.f_reg_addr, buffer(3, 2))
 
-        elseif (stage.value == stage_types.DATA) then
+        elseif (stage.value == ControlTransferStage.DATA) then
             -- For future use
             local reg_value = buffer(0, 4):le_uint()
 
             t_fl2k:add_le(f.f_reg_value, buffer(0, 4))
 
-        elseif (stage == stage_types.STATUS) then
+        elseif (stage == ControlTransferStage.STATUS) then
             -- Do nothing
         end
-    elseif (pinfo.match_uint == INTERRUPT_AV) then
+    elseif (transfer.value == TransferType.INTERRUPT) then
         pinfo.cols["info"]:set("FL2000 Interrupt")
         t_fl2k:add_le(f.f_irq, buffer(0, 1))
+    elseif (transfer.value == TransferType.BULK) then
+        pinfo.cols["info"]:set("FL2000 Data")
+        -- TODO: is it possible to parse it as image?
+    else
+        pinfo.cols["info"]:set("FL2000 WTF???")
     end
 end
 
-usb_table = DissectorTable.get("usb.control")
-usb_table:add(CONTROL_UNKNOWN, fl2k_proto) -- Maps to UNKNOWN InterfaceClass
+usb_control_table = DissectorTable.get("usb.control")
+usb_control_table:add(InterfaceClass.UNKNOWN, fl2k_proto)
 
-usb_table = DissectorTable.get("usb.interrupt")
-usb_table:add(INTERRUPT_AV, fl2k_proto) -- Maps to Audio/Video InterfaceClass
+usb_interrupt_table = DissectorTable.get("usb.interrupt")
+usb_interrupt_table:add(InterfaceClass.AV, fl2k_proto)
+
+usb_bulk_table = DissectorTable.get("usb.bulk")
+usb_bulk_table:add(InterfaceClass.AV, fl2k_proto)
