662b75b47b4a2048750388fc9e6aa37f64330868
Artem Mygaiev <joculator@gmail.com>
2019-05-18 16:04:01 +0300

    Move includes and custom I2C class to header

--- bridge/it66121.h
+++ bridge/it66121.h
@@ -8,6 +8,24 @@
 #ifndef __IT66121_H__
 #define __IT66121_H__
 
+#include <linux/device.h>
+#include <linux/module.h>
+
+#include <linux/hdmi.h>
+#include <linux/i2c.h>
+#include <linux/component.h>
+#include <linux/regmap.h>
+
+#include <drm/drmP.h>
+#include <drm/drm_atomic.h>
+#include <drm/drm_atomic_helper.h>
+#include <drm/drm_crtc_helper.h>
+#include <drm/drm_simple_kms_helper.h>
+#include <drm/drm_edid.h>
+
 #include "it66121_registers.h"
+
+/* Custom code for DRM bridge autodetection since there is no DT support */
+#define I2C_CLASS_HDMI	(1<<9)
 
 #endif /* __IT66121_H__ */
--- bridge/it66121_drv.c
+++ bridge/it66121_drv.c
@@ -6,21 +6,6 @@
  * (C) Copyright 2018-2019, Artem Mygaiev
  */
 
-#include <linux/device.h>
-#include <linux/module.h>
-
-#include <linux/hdmi.h>
-#include <linux/i2c.h>
-#include <linux/component.h>
-#include <linux/regmap.h>
-
-#include <drm/drmP.h>
-#include <drm/drm_atomic.h>
-#include <drm/drm_atomic_helper.h>
-#include <drm/drm_crtc_helper.h>
-#include <drm/drm_simple_kms_helper.h>
-#include <drm/drm_edid.h>
-
 #include "it66121.h"
 
 #define VENDOR_ID	0x4954
@@ -31,8 +16,6 @@
 #define OFFSET_BITS	8
 #define VALUE_BITS	8
 
-/* Custom code for DRM bridge autodetection since there is no DT support */
-#define I2C_CLASS_HDMI	(1<<9)
 
 struct it66121_priv {
 	struct regmap *regmap;
