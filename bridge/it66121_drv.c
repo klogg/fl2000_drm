f92b651d3cca83d6780b18027ba53c9a1a937106
Artem Mygaiev <joculator@gmail.com>
2019-05-30 13:22:21 +0300

    Continued implementation for IT66121 driver
     - Some registers renamed/redefined
     - Added switchable register banks
     - Implemented EDID reading (without original workaround for now)
     - Added initial modes configuration based on EDID
    
    Compile-tested only, validation and testing needed

--- bridge/it66121_drv.c
+++ bridge/it66121_drv.c
@@ -16,6 +16,10 @@
 #define OFFSET_BITS	8
 #define VALUE_BITS	8
 
+enum it66121_intr_state {
+	RUN = (1U),
+	STOP = (0U),
+};
 
 struct it66121_priv {
 	struct regmap *regmap;
@@ -23,6 +27,15 @@ struct it66121_priv {
 	struct drm_bridge bridge;
 	struct drm_connector connector;
 	enum drm_connector_status status;
+
+	struct work_struct work;
+	struct workqueue_struct *work_queue;
+	atomic_t state;
+
+	struct regmap_field *sys_status_int;
+	struct regmap_field *sys_status_hpd;
+	struct regmap_field *ddc_status_done;
+	struct regmap_field *ddc_status_error;
 };
 
 static int it66121_remove(struct i2c_client *client);
@@ -30,6 +43,21 @@ static int it66121_remove(struct i2c_client *client);
 /* According to datasheet IT66121 addresses are 0x98 or 0x9A including cmd */
 static const unsigned short it66121_addr[] = {(0x98 >> 1), (0x9A >> 1),
 	I2C_CLIENT_END};
+
+static const struct regmap_range_cfg it66121_regmap_banks[] = {
+	/* Do not put common registers to any range, this will lead to skipping
+	 * "bank" configuration when accessing those at addresses 0x00-0x2F */
+	{
+		.name = "Banks",
+		.range_min = IT66121_BANK_START,
+		.range_max = IT66121_BANK_END,
+		.selector_reg = IT66121_SYS_CONTROL,
+		.selector_mask = 3,
+		.selector_shift = 0,
+		.window_start = IT66121_BANK_START,
+		.window_len = IT66121_BANK_SIZE,
+	},
+};
 
 static const struct regmap_config it66121_regmap_config = {
 	.val_bits = 8, /* 8-bit register size */
@@ -45,6 +73,9 @@ static const struct regmap_config it66121_regmap_config = {
 	//.reg_defaults = it66121_reg_defaults,
 	//.num_reg_defaults = ARRAY_SIZE(it66121_reg_defaults),
 
+	.ranges = it66121_regmap_banks,
+	.num_ranges = ARRAY_SIZE(it66121_regmap_banks),
+
 	.reg_format_endian = REGMAP_ENDIAN_BIG,
 	.val_format_endian = REGMAP_ENDIAN_BIG,
 
@@ -52,19 +83,259 @@ static const struct regmap_config it66121_regmap_config = {
 	.use_single_write = true,
 };
 
+#define EDID_SLEEP	1000
+#define EDID_TIMEOUT	200000
+
+#define EDID_HDCP_ADDR	0x74
+#define EDID_DDC_ADDR	0xA0
+
+#define EDID_FIFO_SIZE	32
+
+enum {
+	DDC_CMD_BURST_READ = 0x0,
+	DDC_CMD_LINK_CHECK = 0x2,
+	DDC_CMD_EDID_READ = 0x3,
+	DDC_CMD_ASKV_WRITE = 0x4,
+	DDC_CMD_AINFO_WRITE = 0x5,
+	DDC_CMD_AN_WRITE = 0x6,
+	DDC_CMD_FIFO_CLEAR = 0x9,
+	DDC_CMD_SCL_PULSE = 0xA,
+	DDC_CMD_ABORT = 0xF,
+} ddc_cmd;
+
+static inline int it66121_wait_ddc_ready(struct it66121_priv *priv)
+{
+	int res, val;
+
+	res = regmap_field_read_poll_timeout(priv->ddc_status_done, val, true,
+			EDID_SLEEP, EDID_TIMEOUT);
+	if (res != 0)
+		return res;
+
+	res = regmap_field_read(priv->ddc_status_error, &val);
+	if (res != 0)
+		return res;
+	if (val != 0)
+		return -EIO;
+
+	return 0;
+}
+
+static int it66121_clear_ddc_fifo(struct it66121_priv *priv)
+{
+	int res;
+	unsigned int val;
+
+	res = regmap_read(priv->regmap, IT66121_DDC_CONTROL, &val);
+	if (res != 0)
+		return res;
+
+	res = regmap_write(priv->regmap, IT66121_DDC_CONTROL,
+			IT66121_DDC_MASTER_DDC |
+			IT66121_DDC_MASTER_HOST);
+	if (res != 0)
+		return res;
+
+	res = regmap_write(priv->regmap, IT66121_DDC_COMMAND,
+			DDC_CMD_FIFO_CLEAR);
+	if (res != 0)
+		return res;
+
+	res = regmap_write(priv->regmap, IT66121_DDC_CONTROL, val);
+	if (res != 0)
+		return res;
+
+	return 0;
+}
+
+static int it66121_abort_ddc_ops(struct it66121_priv *priv)
+{
+	int i, res;
+	unsigned int val;
+
+	/* XXX: Prior to DDC abort command there was also a reset of HDCP:
+	 *  1. HDCP_DESIRE clear bit CP DESIRE
+	 *  2. SW_RST set bit HDCP_RST
+	 * it seems wrong to keep them reset, i.e. without restoring initial
+	 * state, but somehow this is how it was implemented. This sequence is
+	 * removed since HDCP is not supported */
+
+	res = regmap_read(priv->regmap, IT66121_DDC_CONTROL, &val);
+	if (res != 0)
+		return res;
+
+	res = regmap_write(priv->regmap, IT66121_DDC_CONTROL,
+			IT66121_DDC_MASTER_DDC |
+			IT66121_DDC_MASTER_HOST);
+	if (res != 0)
+		return res;
+
+	/* According to 2009/01/15 modification by Jau-Chih.Tseng@ite.com.tw
+	 * do abort DDC twice */
+	for (i = 0; i < 2; i++) {
+		res = regmap_write(priv->regmap, IT66121_DDC_COMMAND,
+				DDC_CMD_ABORT);
+		if (res != 0)
+			return res;
+
+		res = it66121_wait_ddc_ready(priv);
+		if (res != 0)
+			return res;
+	}
+
+	res = regmap_write(priv->regmap, IT66121_DDC_CONTROL, val);
+	if (res != 0)
+		return res;
+
+	return 0;
+}
+
+static void it66121_intr_work(struct work_struct *work_item)
+{
+	int res;
+	unsigned int val;
+	struct it66121_priv *priv = container_of(work_item, struct it66121_priv,
+			work);
+
+	/* TODO: use mutex */
+
+	res = regmap_field_read(priv->sys_status_int, &val);
+	if (res < 0)
+		/* TODO: Signal error here somehow */
+		return;
+
+	/* XXX: There are at least 5 registers that can source interrupt:
+	 *  - 0x06 (IT66121_INT_STATUS_1)
+	 *  - 0x07 (IT66121_INT_STATUS_2)
+	 *  - 0x08 (IT66121_INT_STATUS_3)
+	 *  - 0xEE (IT66121_EXT_INT_STATUS_1)
+	 *  - 0xF0 (IT66121_EXT_INT_STATUS_2)
+	 * For now we process only DDC events of the IT66121_INT_STATUS_1 which
+	 * implies proper masks configuration */
+	if (val == true) {
+		it666121_int_status_1_reg status_1;
+		res = regmap_read(priv->regmap, IT66121_INT_STATUS_1,
+				&status_1.val);
+		if (res < 0)
+			/* TODO: Signal error here somehow */
+			return;
+		if (status_1.ddc_fifo_err)
+			it66121_clear_ddc_fifo(priv);
+		if (status_1.ddc_bus_hang)
+			it66121_abort_ddc_ops(priv);
+	}
+
+	if (atomic_read(&priv->state) != RUN)
+		return;
+
+	INIT_WORK(&priv->work, &it66121_intr_work);
+
+	queue_work(priv->work_queue, &priv->work);
+}
+
+static int it66121_get_edid_block(void *data, u8 *buf, unsigned int block,
+		size_t len)
+{
+	int res, val, offset = 0, remain = len;
+
+	struct it66121_priv *priv = data;
+
+	/* XXX: Probably we may need to
+	 *  - abort running DDC operation
+	 *  - clear DDC FIFO */
+
+	do {
+		int i;
+		int size = (remain > EDID_FIFO_SIZE) ? EDID_FIFO_SIZE : remain;
+
+		/* Set PC master */
+		res = regmap_write(priv->regmap, IT66121_DDC_CONTROL,
+				IT66121_DDC_MASTER_DDC |
+				IT66121_DDC_MASTER_HOST);
+		if (res != 0)
+			return res;
+
+		/* Clear DDC FIFO */
+		res = regmap_write(priv->regmap, IT66121_DDC_COMMAND,
+				DDC_CMD_FIFO_CLEAR);
+		if (res != 0)
+			return res;
+
+		res = it66121_wait_ddc_ready(priv);
+		if (res != 0)
+			return res;
+
+		res = regmap_write(priv->regmap, IT66121_DDC_ADDRESS,
+				EDID_DDC_ADDR);
+		if (res != 0)
+			return res;
+		res = regmap_write(priv->regmap, IT66121_DDC_OFFSET, offset);
+		if (res != 0)
+			return res;
+		res = regmap_write(priv->regmap, IT66121_DDC_SIZE, size);
+		if (res != 0)
+			return res;
+		res = regmap_write(priv->regmap, IT66121_DDC_SEGMENT, block);
+		if (res != 0)
+			return res;
+		res = regmap_write(priv->regmap, IT66121_DDC_COMMAND,
+				DDC_CMD_EDID_READ);
+		if (res != 0)
+			return res;
+
+		res = it66121_wait_ddc_ready(priv);
+		if (res != 0)
+			return res;
+
+		for (i = 0; i < size; i++) {
+			res = regmap_read(priv->regmap, IT66121_DDC_RD_FIFO,
+					&val);
+			if (res != 0)
+				return res;
+
+			*(buf++) = val & 0xFF;
+		}
+
+		remain -= size;
+		offset += size;
+	} while (remain > 0);
+
+	return 0;
+}
+
+
 static int it66121_connector_get_modes(struct drm_connector *connector)
 {
-	/* 1. drm_do_get_edid
-	 * 2. drm_add_edid_modes
-	 * 3. drm_mode_connector_update_edid_property */
+	int res;
+	struct edid *edid;
+	struct it66121_priv *priv = container_of(connector, struct it66121_priv,
+			connector);
+
+	edid = drm_do_get_edid(connector, it66121_get_edid_block, priv);
+	if (edid == NULL)
+		return -ENOMEM;
+
+	res = drm_add_edid_modes(connector, edid);
+	if (res != 0)
+		return res;
+
+	res = drm_connector_update_edid_property(connector, edid);
+	if (res != 0)
+		return res;
+
 	return 0;
 }
 
 static int it66121_connector_detect_ctx(struct drm_connector *connector,
 		struct drm_modeset_acquire_ctx *ctx, bool force)
 {
-	/* TODO: check connected monitor */
-	return connector_status_disconnected;
+	int res, val;
+	struct it66121_priv *priv = container_of(connector, struct it66121_priv,
+			connector);
+
+	res = regmap_field_read(priv->sys_status_hpd, &val);
+	return val == 0 ? connector_status_disconnected :
+			connector_status_connected;
 }
 
 static struct drm_connector_helper_funcs it66121_connector_helper_funcs = {
@@ -155,7 +426,7 @@ static void it66121_bridge_enable(struct drm_bridge *bridge)
 	regmap_write_bits(priv->regmap, IT66121_SW_RESET, (1<<5), (1<<5));
 
 	/* Power up according to IT66121 manual */
-	regmap_write_bits(priv->regmap, IT66121_SYS_CONTROL_2, (1<<6), (0<<6));
+	regmap_write_bits(priv->regmap, IT66121_SYS_CONTROL, (1<<6), (0<<6));
 	regmap_write_bits(priv->regmap, IT66121_INT_CONTROL, (1<<0), (0<<0));
 	regmap_write_bits(priv->regmap, IT66121_AFE_DRV_CONTROL, (1<<5), (0<<5));
 	regmap_write_bits(priv->regmap, IT66121_AFE_XP_CONTROL, (1<<2)|(1<<6), (0<<2)|(0<<6));
@@ -182,7 +453,7 @@ static void it66121_bridge_disable(struct drm_bridge *bridge)
 
 static void it66121_bridge_mode_set(struct drm_bridge *bridge,
 		struct drm_display_mode *mode,
-		struct drm_display_mode 	*adjusted_mode)
+		struct drm_display_mode *adjusted_mode)
 
 {
 	/*
@@ -207,7 +478,7 @@ static int it66121_probe(struct i2c_client *client)
 
 	dev_info(&client->dev, "Probing IT66121 client");
 
-	priv = kzalloc(sizeof(*priv), GFP_KERNEL);
+	priv = devm_kzalloc(&client->dev, sizeof(*priv), GFP_KERNEL);
 	if (IS_ERR_OR_NULL(priv)) {
 		dev_err(&client->dev, "Cannot allocate IT66121 client private" \
 				"structure");
@@ -223,6 +494,34 @@ static int it66121_probe(struct i2c_client *client)
 
 	priv->bridge.funcs = &it66121_bridge_funcs;
 
+	priv->sys_status_int = devm_regmap_field_alloc(&client->dev,
+			priv->regmap, IT66121_SYS_STATUS_int);
+	if (IS_ERR(priv->sys_status_int)) {
+		ret = PTR_ERR(priv->sys_status_int);
+		goto error;
+	}
+
+	priv->sys_status_hpd = devm_regmap_field_alloc(&client->dev,
+			priv->regmap, IT66121_SYS_STATUS_hpd);
+	if (IS_ERR(priv->sys_status_hpd)) {
+		ret = PTR_ERR(priv->sys_status_hpd);
+		goto error;
+	}
+
+	priv->ddc_status_done = devm_regmap_field_alloc(&client->dev,
+			priv->regmap, IT66121_DDC_STATUS_done);
+	if (IS_ERR(priv->ddc_status_done)) {
+		ret = PTR_ERR(priv->ddc_status_done);
+		goto error;
+	}
+
+	priv->ddc_status_error = devm_regmap_field_alloc(&client->dev,
+			priv->regmap, IT66121_DDC_STATUS_error);
+	if (IS_ERR(priv->ddc_status_error)) {
+		ret = PTR_ERR(priv->ddc_status_error);
+		goto error;
+	}
+
 	drm_bridge_add(&priv->bridge);
 
 	/* Important and somewhat unsafe - bridge pointer is in device structure
@@ -235,6 +534,16 @@ static int it66121_probe(struct i2c_client *client)
 		dev_err(&client->dev, "Cannot register IT66121 component");
 		goto error;
 	}
+
+	/* Setup work queue for interrupt processing work */
+	priv->work_queue = create_workqueue("work_queue");
+	if (IS_ERR_OR_NULL(priv->work_queue)) {
+		dev_err(&client->dev, "Create interrupt workqueue failed");
+		ret = PTR_ERR(priv->work_queue);
+		goto error;
+	}
+
+	it66121_intr_work(&priv->work);
 
 	return 0;
 
@@ -255,6 +564,8 @@ static int it66121_remove(struct i2c_client *client)
 
 	if (IS_ERR_OR_NULL(priv))
 		return 0;
+
+	/*TODO: Cancel pending work and destroy workqueue */
 
 	component_del(&client->dev, &it66121_component_ops);
 
--- bridge/it66121_registers.h
+++ bridge/it66121_registers.h
@@ -13,8 +13,9 @@
 #ifndef __FL2000_REGISTERS_H__
 #define __FL2000_REGISTERS_H__
 
-#define IT66121_BANK_0_OFFSET		0x00
+#define IT66121_BANK_SIZE		0x100
 
+/* 000 - 02F Common registers, same as 100 - 12F */
 #define IT66121_VENDOR_ID_1		0x00
 #define IT66121_VENDOR_ID_2		0x01
 #define IT66121_DEVICE_ID_1		0x02
@@ -22,6 +23,21 @@
 #define IT66121_SW_RESET		0x04
 #define IT66121_INT_CONTROL		0x05
 #define IT66121_INT_STATUS_1		0x06
+typedef union {
+	u32 val;
+	struct {
+		u32 __unused:24;
+		u32 aud_overflow:1;
+		u32 romacq_noack:1;
+		u32 ddc_noack:1;
+		u32 ddc_fifo_err:1;
+		u32 romacq_bus_hang:1;
+		u32 ddc_bus_hang:1;
+		u32 rx_sense:1;
+		u32 hpd_plug:1;
+	} __attribute__ ((aligned, packed));
+} it666121_int_status_1_reg;
+
 #define IT66121_INT_STATUS_2		0x07
 #define IT66121_INT_STATUS_3		0x08
 #define IT66121_INT_MASK_1		0x09
@@ -29,15 +45,31 @@
 #define IT66121_INT_MASK_3		0x0B
 #define IT66121_INT_CLEAR_1		0x0C
 #define IT66121_INT_CLEAR_2		0x0D
-#define IT66121_SYS_CONTROL_1		0x0E
-#define IT66121_SYS_CONTROL_2		0x0F
+#define IT66121_SYS_STATUS		0x0E
+static const struct reg_field IT66121_SYS_STATUS_int =
+		REG_FIELD(IT66121_SYS_STATUS, 7, 7);
+static const struct reg_field IT66121_SYS_STATUS_hpd =
+		REG_FIELD(IT66121_SYS_STATUS, 6, 6);
+
+#define IT66121_SYS_CONTROL		0x0F
+
 #define IT66121_DDC_CONTROL		0x10
+#define IT66121_DDC_MASTER_ROM		(1<<1)
+#define IT66121_DDC_MASTER_DDC		(0<<1)
+#define IT66121_DDC_MASTER_HOST		(1<<0)
+#define IT66121_DDC_MASTER_HDCP		(0<<0)
+
 #define IT66121_DDC_ADDRESS		0x11
 #define IT66121_DDC_OFFSET		0x12
-#define IT66121_DDC_BYTE		0x13
+#define IT66121_DDC_SIZE		0x13
 #define IT66121_DDC_SEGMENT		0x14
 #define IT66121_DDC_COMMAND		0x15
 #define IT66121_DDC_STATUS		0x16
+static const struct reg_field IT66121_DDC_STATUS_done =
+		REG_FIELD(IT66121_DDC_STATUS, 7, 7);
+static const struct reg_field IT66121_DDC_STATUS_error =
+		REG_FIELD(IT66121_DDC_STATUS, 3, 5);
+
 #define IT66121_DDC_RD_FIFO		0x17
 /* reserved */
 #define IT66121_HDCP_ADDRESS		0x19
@@ -46,104 +78,122 @@
 #define IT66121_ROM_STATUS		0x1C
 /* reserved */
 /* reserved */
-/* 1F - 57 HDCP registers, ignored */
-#define IT66121_MCLK_CONTROL		0x58
-#define IT66121_PLL_CONTROL		0x59
-#define IT66121_CLK_POWER_CONTROL	0x5A
-#define IT66121_OS_FREQ_NUM_2		0x5B
-#define IT66121_OS_FREQ_NUM_1		0x5C
-/* reserved */
-#define IT66121_TX_CLK_COUNT		0x5E
-#define IT66121_PLL_LOCK_STATUS		0x5F
-#define IT66121_AUDIO_FREQ_COUNT	0x60
-#define IT66121_AFE_DRV_CONTROL		0x61
-#define IT66121_AFE_XP_CONTROL		0x62
-#define IT66121_AFE_IP_CONTROL_2	0x63
-#define IT66121_AFE_IP_CONTROL_1	0x64
-#define IT66121_AFE_RING_CONTROL	0x65
-#define IT66121_AFE_DRV_HS		0x66
-/* reserved */
-#define IT66121_AFE_IP_CONTROL_3	0x68
-#define IT66121_AFE_PAT_RSTB		0x69
-#define IT66121_AFE_XP_TEST		0x6A
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-#define IT66121_INPUT_MODE		0x70
-#define IT66121_INPUT_IO_CONTROL	0x71
-#define IT66121_INPUT_COLOR_CONV	0x72
-#define IT66121_Y_BLANK_LEVEL		0x73
-#define IT66121_C_BLANK_LEVEL		0x74
-#define IT66121_RGB_BLANK_LEVEL		0x75
-#define IT66121_MATRIX_11V_1		0x76
-#define IT66121_MATRIX_11V_2		0x77
-#define IT66121_MATRIX_12V_1		0x78
-#define IT66121_MATRIX_12V_2		0x79
-#define IT66121_MATRIX_13V_1		0x7A
-#define IT66121_MATRIX_13V_2		0x7B
-#define IT66121_MATRIX_21V_1		0x7C
-#define IT66121_MATRIX_21V_2		0x7D
-#define IT66121_MATRIX_22V_1		0x7E
-#define IT66121_MATRIX_22V_2		0x7F
-#define IT66121_MATRIX_23V_1		0x80
-#define IT66121_MATRIX_23V_2		0x81
-#define IT66121_MATRIX_31V_1		0x82
-#define IT66121_MATRIX_31V_2		0x83
-#define IT66121_MATRIX_32V_1		0x84
-#define IT66121_MATRIX_32V_2		0x85
-#define IT66121_MATRIX_33V_1		0x86
-#define IT66121_MATRIX_33V_2		0x87
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-#define IT66121_PCI2C_CEC_ADDRESS	0x8D
-/* reserved */
-/* reserved */
-/* 90 - BF Pattern generation registers, ignored */
-#define IT66121_HDMI_MODE		0xC0
-#define IT66121_HDMI_AV_MUTE		0xC1
-#define IT66121_HDMI_BLACK_SRC		0xC2
-#define IT66121_HDMI_OESS_PREIOD	0xC3
-/* reserved */
-#define IT66121_HDMI_AUDIO_CTS		0xC5
-#define IT66121_HDMI_GEN_CTRL_PACKET	0xC6
-/* reserved */
-/* reserved */
-#define IT66121_HDMI_NULL_PACKET	0xC9
-#define IT66121_HDMI_ACP_PACKET		0xCA
-/* reserved */
-/* reserved */
-#define IT66121_HDMI_AVI_INFO_PACKET	0xCD
-#define IT66121_HDMI_AUDIO_INFO_PACKET	0xCE
-/* reserved */
-#define IT66121_HDMI_MPEG_INFO_PACKET	0xD0
-#define IT66121_HDMI_VIDEO_PARAM_STATUS	0xD1
-#define IT66121_HDMI_3D_INFO_PACKET	0xD2
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-#define IT66121_HDMI_PCLK_CONTROL	0xD7
-#define IT66121_HDMI_PCLK_COUNT		0xD8
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-/* reserved */
-/* E0 - F2 Audio channel registers, ignored */
-/* F3 - F8 Test registers, ignored */
+/* 01F - 2F HDCP registers, ignored */
 
-#define IT66121_BANK_1_OFFSET		0x01
+#define IT66121_BANK_START		0x30
 
-/* 30 - 8F Packet content registers, ignored */
-/* 90 - BF Audio channel status registers, ignored */
+/* 030 - 0FF Bank 0 */
+/* 030 - 057 HDCP registers, ignored */
+#define IT66121_MCLK_CONTROL		0x058
+#define IT66121_PLL_CONTROL		0x059
+#define IT66121_CLK_POWER_CONTROL	0x05A
+#define IT66121_OS_FREQ_NUM_2		0x05B
+#define IT66121_OS_FREQ_NUM_1		0x05C
+/* reserved */
+#define IT66121_TX_CLK_COUNT		0x05E
+#define IT66121_PLL_LOCK_STATUS		0x05F
+#define IT66121_AUDIO_FREQ_COUNT	0x060
+#define IT66121_AFE_DRV_CONTROL		0x061
+#define IT66121_AFE_XP_CONTROL		0x062
+#define IT66121_AFE_IP_CONTROL_2	0x063
+#define IT66121_AFE_IP_CONTROL_1	0x064
+#define IT66121_AFE_RING_CONTROL	0x065
+#define IT66121_AFE_DRV_HS		0x066
+/* reserved */
+#define IT66121_AFE_IP_CONTROL_3	0x068
+#define IT66121_AFE_PAT_RSTB		0x069
+#define IT66121_AFE_XP_TEST		0x06A
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+#define IT66121_INPUT_MODE		0x070
+#define IT66121_INPUT_IO_CONTROL	0x071
+#define IT66121_INPUT_COLOR_CONV	0x072
+#define IT66121_Y_BLANK_LEVEL		0x073
+#define IT66121_C_BLANK_LEVEL		0x074
+#define IT66121_RGB_BLANK_LEVEL		0x075
+#define IT66121_MATRIX_11V_1		0x076
+#define IT66121_MATRIX_11V_2		0x077
+#define IT66121_MATRIX_12V_1		0x078
+#define IT66121_MATRIX_12V_2		0x079
+#define IT66121_MATRIX_13V_1		0x07A
+#define IT66121_MATRIX_13V_2		0x07B
+#define IT66121_MATRIX_21V_1		0x07C
+#define IT66121_MATRIX_21V_2		0x07D
+#define IT66121_MATRIX_22V_1		0x07E
+#define IT66121_MATRIX_22V_2		0x07F
+#define IT66121_MATRIX_23V_1		0x080
+#define IT66121_MATRIX_23V_2		0x081
+#define IT66121_MATRIX_31V_1		0x082
+#define IT66121_MATRIX_31V_2		0x083
+#define IT66121_MATRIX_32V_1		0x084
+#define IT66121_MATRIX_32V_2		0x085
+#define IT66121_MATRIX_33V_1		0x086
+#define IT66121_MATRIX_33V_2		0x087
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+#define IT66121_PCI2C_CEC_ADDRESS	0x08D
+/* reserved */
+/* reserved */
+/* 090 - 0BF Pattern generation registers, ignored */
+#define IT66121_HDMI_MODE		0x0C0
+#define IT66121_HDMI_AV_MUTE		0x0C1
+#define IT66121_HDMI_BLACK_SRC		0x0C2
+#define IT66121_HDMI_OESS_PREIOD	0x0C3
+/* reserved */
+#define IT66121_HDMI_AUDIO_CTS		0x0C5
+#define IT66121_HDMI_GEN_CTRL_PACKET	0x0C6
+/* reserved */
+/* reserved */
+#define IT66121_HDMI_NULL_PACKET	0x0C9
+#define IT66121_HDMI_ACP_PACKET		0x0CA
+/* reserved */
+/* reserved */
+#define IT66121_HDMI_AVI_INFO_PACKET	0x0CD
+#define IT66121_HDMI_AUDIO_INFO_PACKET	0x0CE
+/* reserved */
+#define IT66121_HDMI_MPEG_INFO_PACKET	0x0D0
+#define IT66121_HDMI_VIDEO_PARAM_STATUS	0x0D1
+#define IT66121_HDMI_3D_INFO_PACKET	0x0D2
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+#define IT66121_HDMI_PCLK_CONTROL	0x0D7
+#define IT66121_HDMI_PCLK_COUNT		0x0D8
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+/* reserved */
+/* E0 - E7 Audio channel registers, ignored */
+#define IT66121_EXT_INT_CONTROL		0x0E8
+/* reserved */
+/* reserved */
+/* reserved */
+#define IT66121_EXT_INT_MASK		0x0EC
+/* reserved */
+#define IT66121_EXT_INT_STATUS_1	0x0EE
+/* reserved */
+#define IT66121_EXT_INT_STATUS_2	0x0F0
+/* reserved */
+/* reserved */
+/* 0F3 - 0F8 Test registers, ignored */
+/* 0F9 - 0FF undefined*/
 
-/*  CEC registers bank, ignored */
+/* 130 - 1BF Bank 1 */
+/* 130 - 18F HDMI packet content registers, ignored */
+/* 190 - 1BF Audio channel status registers, ignored */
+
+#define IT66121_BANK_END		0x1FF
+
+/* CEC registers ignored (must be configured as a separate regmap / device) */
 
 #endif /* __FL2000_REGISTERS_H__ */
