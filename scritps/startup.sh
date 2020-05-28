a570089a97cd81ff6ac46f4415ad5e0d99cf20b0
Artem Mygaiev <joculator@gmail.com>
2020-05-28 22:14:55 +0300

    Capture more buffers for debug

--- scritps/startup.sh
+++ scritps/startup.sh
@@ -7,7 +7,7 @@ modprobe usbmon
 modprobe drm
 modprobe drm_kms_helper
 
-tcpdump -i usbmon2 -nn -w /tmp/usb.pcap -c 10000 &
+tcpdump -i usbmon2 -nn -w /tmp/usb.pcap -c 20000 &
 
 insmod fl2000.ko
 insmod it66121.ko
