55544eacc7cbd9db43fd45ac836b544bc2225f7a
Artem Mygaiev <joculator@gmail.com>
2024-05-09 10:48:05 -0700

    Fixed alignment & line width in formatting script
    Clang-fromat default linux configuration is extended with following:
      AlignConsecutiveMacros: true
      AlignConsecutiveBitFields: true
    In addition, column limit set to 100 as it is default in Linux kernel:
      ColumnLimit: 100
    
    All changes above are done with sed and not bulletproof.
    
    In addition, all extra parameters to script are now passed to
    clang-format command line
    
    Downloaded .clang-format is added to gitignore to avoid noise in status

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -4,16 +4,14 @@ mod_pattern="*.mod.c"
 
 if [ ! -f .clang-format ]; then
 	wget https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain/.clang-format
+	sed -i '/AlignConsecutiveAssignments/i AlignConsecutiveMacros: true\nAlignConsecutiveBitFields: true' .clang-format
+	sed -i 's/\(ColumnLimit: \)[0-9]*/\1100/g' .clang-format
 fi
-
-# TODO: Add following:
-#   AlignConsecutiveMacros: true
-#   AlignConsecutiveBitFields: true
 
 for file in {,bridge/}*.[ch]
 do
 	if [[ $file == $mod_pattern ]]; then
 		continue
 	fi
-	clang-format -i "$file"
+	clang-format $@ -i "$file"
 done
