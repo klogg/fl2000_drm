9d9d0dab578b66e649a0ba824565d957e9ae2f54
Matias Zappacosta <matizappacosta@outlook.com>
2026-02-20 13:42:52 -0300

    Fix compatibility with Linux kernel 6.17
    - Replace drm_fbdev_generic.h with drm_fbdev_shmem.h
    - Remove deprecated .lastclose and .date from drm_driver
    - Add DRM_FBDEV_SHMEM_DRIVER_OPS macro
    - Update bridge_attach signature (add encoder parameter)
    - Update mode_valid signature (add const)
    - Migrate from struct edid to struct drm_edid API:
      - drm_do_get_edid -> drm_edid_read_custom
      - drm_connector_update_edid_property -> drm_edid_connector_update
      - drm_detect_hdmi_monitor -> drm_edid_is_digital
      - drm_add_edid_modes -> drm_edid_connector_add_modes
      - kfree -> drm_edid_free
    
    Tested on Ubuntu 24.04 with kernel 6.17.0-14-generic

--- fl2000.h
+++ fl2000.h
@@ -30,7 +30,7 @@
 #include <drm/drm_fourcc.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_framebuffer.h>
-#include <drm/drm_fbdev_generic.h>
+#include <drm/drm_fbdev_shmem.h>
 #include <drm/drm_gem_framebuffer_helper.h>
 #include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_atomic_helper.h>
