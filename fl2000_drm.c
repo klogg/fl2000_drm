3288165f3f5e18dfc7647149da22a2da35913e63
Jie Feng <hjf3108@gmail.com>
2026-09-14 15:31:10 +0800

    Fix mode calculation error, sign is always 0, so all the modes tested are the same

--- fl2000_drm.c
+++ fl2000_drm.c
@@ -215,7 +215,7 @@ static int fl2000_mode_calc(const struct drm_display_mode *mode,
 	 * 0, -1, 1, -2, 2, -3, 3, -3, 4, -4, 5, -5, ...
 	 * Here, 's' is used for sign, 'm' is used for modulo, and 'd' is the adjustment value
 	 */
-	for (int m = 0, s = 0, d = 0; m <= max_h_adjustment * 2; m++, s = -s, d += m * s) {
+	for (int m = 0, s = 1, d = 0; m <= max_h_adjustment * 2; m++, s = -s, d += m * s) {
 		/* Maximum pixel clock 1GHz, or 10^9Hz. Multiply by 10^6 we get 10^15Hz. Assume
 		 * maximum htotal is 10000 pix (no way) we get 10^19 max value and using u64 which
 		 * is 1.8*10^19 no overflow can occur. Assume all this was checked before
